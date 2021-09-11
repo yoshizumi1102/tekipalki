3cac13f564508d96331485bb4103e22320ad0d70
yoshizumi <yoshizumi1102@users.noreply.github.com>
2021-09-11 21:23:25 +0900

    コンタクトまで

--- index.php
+++ index.php
@@ -5,9 +5,130 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TEKIPAKI</title>
   <meta name="description" content="TEKIPAKIは、Webサイト制作やLP制作、その他Webコンテンツの制作をお手伝いする個人事業主です。">
-  meta
+  <link rel="stylesheet" href="/assets/css/reset.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-  
+  <header>
+    <div class="header-menu">
+      <div class="menu-btn">
+        <img src="" alt="">
+      </div>
+        <nav class="header-navi">
+          <ul class="header-nav__list">
+            <li class="nav__list-item">TOP</li>
+            <li class="nav__list-item">TEKIPAKIについて</li>
+            <li class="nav__list-item">制作実績</li>
+            <li class="nav__list-item">サービス</li>
+            <li class="nav__list-item">お知らせ</li>
+            <li class="nav__list-item">お問い合わせ</li>
+          </ul>
+        </nav>
+    </div>
+  </header>
+  <div class="mv">
+    <div class="mv__inner l-inner">
+    <div class="mv__logo">
+      <img src="./assets/image_uniq/logo.png" alt="">
+    </div>
+      <p class="mv__txt">TEKIPAKIは、Webサイト制作やLP制作、その他Webコンテンツの制作を<br>お手伝いする個人事業主です。</p>
+    </div>
+  </div>
+  <!-- ここからメイン -->
+  <main class="main-wrap wrap-tekipaki">
+
+    <!-- ここからアバウト -->
+    <section class="top-sec top-sec01 top-about">
+      <div class="l-inner">
+        <h2 class="headgingA about__heading">ABOUT TEKIPAKI</h2>
+        <p class="top-about__lead">成果につながる<br>クリエイティブを。</p>
+        <p class="top-about__lead-txt">
+          章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入る
+        </p>
+      </div>
+    </section>
+    <!-- About End -->
+
+    <!-- ここからWORKS -->
+    <section class="top-sec top-sec02 top-works">
+      <div class="l-inner">
+        <h2 class="headingA works__heading">WORKS</h2>
+        <p>TEKIPAKIのこれまでの実績をご紹介します。</p>
+        <div class="works-container">
+          <div class="works-container__img">
+            <img src="./assets/image_uniq/work-img01.jpg" alt="">
+          </div>
+          <div class="works-container__body">
+            <p>HALU AUTO様</p>
+          </div>
+        </div>
+        <div class="page-btn">
+          <a href="" class="btnA">
+            <span class="btnA__inner">実績一覧を見る</span>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!-- WORKS END -->
+
+    <!-- ここからSERVICE -->
+    <section class="top-sec top-sec03 top-service">
+      <div class="l-inner">
+        <h2 class="headingA service__heading">SERVICE</h2>
+        <div class="service-wrapper">
+          <div class="service-wrapper__number">01</div>
+          <div class="service-wrapper__inner">
+            <div class="service-wrapper__body">
+              <h3 class="headingB service-wrapper__heading">Webサイト制作</h3>
+              <p class="service-wrapper__txt">
+                ここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入るここに文章が入る
+              </p>
+              <div class="service-wrapper__btn">
+                <a href="" class="btnA service-btn">
+                  <span class="btnA__inner">制作実績を見る</span>
+                </a>
+                <a href="" class="btnA service-btn">
+                  <span class="btnA__inner">詳細を見る</span>
+                </a>
+              </div>
+            </div>
+            <div class="service-wrapper__img">
+              <img src="./assets/image_uniq/service-img01.jpg" alt="">
+            </div>
+         </div>
+        </div>
+        <div class="page-btn">
+          <a href="" class="btnA">
+            <span class="btnA__inner">サービスを見る</span></a>
+        </div>
+      </div>
+    </section>
+    <!-- SERVICE END -->
+
+    <!-- ここからCONTACT -->
+    <section class="top-sec top-sec04">
+      <div class="l-inner">
+        <h2 class="headingA contact__heading">CONTACT</h2>
+        <p class="top-contact__txt">
+          TEKIPAKIへのお問い合わせは下記お問い合わせフォームまたはお電話番号からお願いいたします。
+        </p>
+        <div class="top-contact__menu">
+          <div class="top-contact__menu__inner">
+            <p class="top-contact__menu__tel">TEL：080−3790−5939</p>
+            <div class="top-contact__menu__btn">
+              <a href="" class="btnA contact-btn">
+                <span class="btnA__inner">
+                  お問合せフォームへ
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- CONTACT END -->
+
+  </main>
+  <!-- main END -->
 </body>
 </html>
