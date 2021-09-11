fb2410d4834500e92d60a1659c8b9b2e873020a9
yoshizumi <yoshizumi1102@users.noreply.github.com>
2021-09-11 14:05:49 +0900

    second commit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TEKIPAKI</title>
+  <meta name="description" content="TEKIPAKIは、Webサイト制作やLP制作、その他Webコンテンツの制作をお手伝いする個人事業主です。">
   meta
 </head>
 <body>
